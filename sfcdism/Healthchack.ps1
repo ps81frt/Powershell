9dc740aa9a232de2c4775901d0581a915b850220
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2023-06-25 13:47:43 +0200

    Create Healthchack.ps1

---
+++ sfcdism/Healthchack.ps1
@@ -0,0 +1,11 @@
+
+# powershell ou CMD ou ADMIN
+
+# Check des systemes de fichiers
+sfc /scannow
+
+# DISM
+Dism /Online /Cleanup-Image /ScanHealth
+Dism /Online /Cleanup-Image /CheckHealth
+Dism /Online /Cleanup-Image /RestoreHealth
+Restart-Computer
