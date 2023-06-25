2aef3da9f33136b19439cd258dc395b9355c1ead
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2023-06-25 13:41:12 +0200

    Update carteresau.ps1

--- carteresau.ps1
+++ carteresau.ps1
@@ -1,5 +1,5 @@
-& {Get-NetAdapter W*i* | fl Dri* && Get-NetAdapterAdvancedProperty W*i* | ft DisplayName, DisplayValue, ValidDisplayValues && Get-NetiPConfiguration && Get-DnsClientServerAddress | Format-Table -AutoSize} | Out-File -append ~/Desktop/reseau.txt
+& {Get-NetAdapter W*i* | fl Dri* && Get-NetAdapterAdvancedProperty W*i* | ft DisplayName, DisplayValue, ValidDisplayValues && Get-NetiPConfiguration && Get-DnsClientServerAddress | Format-Table -AutoSize} | Out-File -append $env:USERPROFILE\Desktop\reseau.txt
 
 
 
- & {Show-NetfirewallRule| sort direction | ? enabled -eq "true" | ft -property @{label="Name" ; expression={$_.displayname}}, @{label="Direction" ; expression={$_.direction}}} | Out-File ~/Desktop/parefeu.txt
+ & {Show-NetfirewallRule| sort direction | ? enabled -eq "true" | ft -property @{label="Name" ; expression={$_.displayname}}, @{label="Direction" ; expression={$_.direction}}} | Out-File $env:USERPROFILE\Desktop\parefeu.txt
