515e9dae320275fd516d0b98ad02d0d15a1c41f1
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2023-11-29 17:52:30 +0100

    Update Healthchack.ps1

--- sfcdism/Healthchack.ps1
+++ sfcdism/Healthchack.ps1
@@ -1,10 +1,17 @@
+# Ouvrir Powershell en administrateur
+
+---------------------------------------------------------------------
 
 # powershell ou CMD ou ADMIN
 
+---------------------------------------------------------------------
+
 # Check des systemes de fichiers
+
 sfc /scannow
 
 # DISM
+
 Dism /Online /Cleanup-Image /ScanHealth
 Dism /Online /Cleanup-Image /CheckHealth
 Dism /Online /Cleanup-Image /RestoreHealth
