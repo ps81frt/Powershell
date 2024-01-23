d4cbc53c62a53bf853999021c3c0557e39a4f8dc
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2024-01-23 19:37:12 +0100

    Create Set-ScreenRefreshRate.ps1

---
+++ RefreshRate/Set-ScreenRefreshRate.ps1
@@ -0,0 +1,22 @@
+function Set-ScreenRefreshRate
+{ 
+    param ( 
+        [Parameter(Mandatory=$true)] 
+        [int] $Frequency
+    ) 
+
+[Display.PrimaryScreen]::ChangeRefreshRate($frequency) 
+
+}
+
+function Get-ScreenRefreshRate
+{
+    $frequency = Get-WmiObject -Class "Win32_VideoController" | Select-Object CurrentRefreshRate
+
+    return $frequency
+}
+$currentFrequency = Get-ScreenRefreshRate
+$newFrequency     = if ($currentFrequency -eq 40) { 60,01 } else { 60,01 }
+
+Set-ScreenRefreshRate -Frequency $newFrequency ;
+Get-ScreenRefreshRate
