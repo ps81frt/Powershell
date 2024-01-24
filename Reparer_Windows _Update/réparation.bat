dcb7947f7b1562110c255577cbb990e789ab24b2
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2024-01-24 06:28:48 +0100

    Update réparation.bat

--- Reparer_Windows _Update/réparation.bat
+++ Reparer_Windows _Update/réparation.bat
@@ -1,3 +1,26 @@
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check pour les permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> Si erreurs, vous n etes pas administrateur.
+if '%errorlevel%' NEQ '0' (
+    echo Requiert les droit Administrateur...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+
 net stop bits
 net stop wuauserv
 net stop cryptsvc
