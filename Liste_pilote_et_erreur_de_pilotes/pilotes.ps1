81471b80fa8a52e004f09e5b9abf3d8f508d8820
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2024-01-24 09:28:00 +0100

    Update pilotes.ps1

--- Liste_pilote_et_erreur_de_pilotes/pilotes.ps1
+++ Liste_pilote_et_erreur_de_pilotes/pilotes.ps1
@@ -1,4 +1,4 @@
-&{mode 300 ; Get-PnpDevice -PresentOnly | Select-Object -Property Status,Friendlyname,InstanceId | Format-Table -GroupBy Status ; 
+powershell.exe -command &{mode 300 ; Get-PnpDevice -PresentOnly | Select-Object -Property Status,Friendlyname,InstanceId | Format-Table -GroupBy Status ; 
 Get-CimInstance Win32_PnPEntity | Select-Object Status, Class, FriendlyName, instanceId | Format-Table -GroupBy Status ;
 Get-WmiObject -Class Win32_PnpEntity -ComputerName localhost -Namespace Root\CIMV2 | Where-Object {$_.ConfigManagerErrorCode -gt 0 } 
 | Select-Object ConfigManagerErrorCode,Errortext,Present,Status,StatusInfo,caption | Format-List -GroupBy Status } 
