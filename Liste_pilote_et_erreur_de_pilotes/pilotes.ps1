42b96b11528c981081506e380115a69e6fb48e27
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2024-01-24 09:28:50 +0100

    Update pilotes.ps1

--- Liste_pilote_et_erreur_de_pilotes/pilotes.ps1
+++ Liste_pilote_et_erreur_de_pilotes/pilotes.ps1
@@ -1,6 +1,6 @@
-powershell.exe -command &{mode 300 ; Get-PnpDevice -PresentOnly | Select-Object -Property Status,Friendlyname,InstanceId | Format-Table -GroupBy Status ; 
+powershell.exe -command "&{mode 300 ; Get-PnpDevice -PresentOnly | Select-Object -Property Status,Friendlyname,InstanceId | Format-Table -GroupBy Status ; 
 Get-CimInstance Win32_PnPEntity | Select-Object Status, Class, FriendlyName, instanceId | Format-Table -GroupBy Status ;
 Get-WmiObject -Class Win32_PnpEntity -ComputerName localhost -Namespace Root\CIMV2 | Where-Object {$_.ConfigManagerErrorCode -gt 0 } 
 | Select-Object ConfigManagerErrorCode,Errortext,Present,Status,StatusInfo,caption | Format-List -GroupBy Status } 
 |Out-File -Path $env:userprofile\Desktop\tspilote_et_erreur.txt ;
-exit
+exit"
