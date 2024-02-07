b43c462b160337f1a56edb64a0cc8c0d62435c2d
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2024-02-07 13:47:04 +0100

    Update get-refreshrate.ps1

--- RefreshRate/get-refreshrate.ps1
+++ RefreshRate/get-refreshrate.ps1
@@ -1,2 +1,1 @@
 &{Get-WmiObject win32_desktopmonitor;Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorBasicDisplayParams;Get-WmiObject win32_videocontroller | select caption, CurrentHorizontalResolution, CurrentVerticalResolution, MaxRefreshRate, MinRefreshRate, currentrefreshrate} | Out-File -FilePath $env:USERPROFILE\Desktop\ecran.txt
- CA81.SSD dans 󰋜 ~ code .\Desktop\ecran.txt
