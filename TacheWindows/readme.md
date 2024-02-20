8a5dd284dcba9080729b95459a3628c591e40ffb
Sh@d0w <stephinvest81@gmail.com>
2024-02-20 19:51:42 +0100

    .

---
+++ TacheWindows/readme.md
@@ -0,0 +1,1 @@
+# Export des tache windows
---
+++ TacheWindows/tachewindows.ps1
@@ -0,0 +1,41 @@
+$backupPath = "C:\TachesWindows"
+
+$taskFolders = (Get-ScheduledTask).TaskPath | Where-Object { ($_ -notmatch "Microsoft") } | Select-Object -Unique
+
+Write-Host "Debut de l exportation des taches." -ForegroundColor Cyan
+
+if (Test-Path -Path $backupPath) {
+    Write-Host "le Fichier existe deja: $backupPath" -ForegroundColor Yellow
+}
+else {
+    New-Item -ItemType Directory -Path $backupPath | Out-Null
+    Write-Host "Chemin Creer: $backupPath" -ForegroundColor Green
+}
+
+foreach ($taskFolder in $taskFolders) {
+    Write-Host "Dossier TachesWindows: $taskFolder" -ForegroundColor Cyan
+
+    if ($taskFolder -ne "\") {
+        $folderPath = "$backupPath$taskFolder"
+
+        if (-not (Test-Path -Path $folderPath)) {
+            New-Item -ItemType Directory -Path $folderPath | Out-Null
+        }
+        else {
+            Write-Host "Le fichier existe deja: $folderPath" -ForegroundColor Yellow
+        }
+    }
+
+    $tasks = Get-ScheduledTask -TaskPath $taskFolder -ErrorAction SilentlyContinue
+
+    foreach ($task in $tasks) {
+        $taskName = $task.TaskName
+
+        # Export the task and save it to a file
+        $taskInfo = Export-ScheduledTask -TaskName $taskName -TaskPath $taskFolder
+        $taskInfo | Out-File "$backupPath$taskFolder$taskName.xml"
+        Write-Host "Saved file $backupPath$taskFolder$taskName.xml" -ForegroundColor Cyan
+    }
+}
+
+Write-Host "Exportation des taches Terminer !!!." -ForegroundColor Green
